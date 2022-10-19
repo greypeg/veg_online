9f623fb302aa48f443a2af4df166234245acb677
greypeg <gpapas028@gmail.com>
2022-10-19 19:53:11 +0100

    commit changes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex justify-center bg-zinc-900 py-4 text-white mx-3">
+        <div className="bg-white-500 py-1"></div>Copyright © 2022 Gregory Papas</div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,20 @@ import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { useRouter } from "next/router";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
   return (
     <SessionProvider session={session}>
+      {router.pathname !== '/signup' && <Navbar />}
       <Component {...pageProps} />
+      {router.pathname !== '/signup' && <Footer /> }
     </SessionProvider>
   );
 };
